59c243851ff731287f2c96b65a5efe320b5e7ba0
zarif-ahnaf <86199070+zarif-ahnaf@users.noreply.github.com>
2024-11-08 18:50:24 +0600

    Add codes

--- Day 10/In lab/queue.cpp
+++ Day 10/In lab/queue.cpp
@@ -3,78 +3,98 @@ using namespace std;
 
 const int MAX_SIZE = 100;
 
-class Queue{
+class Queue
+{
 private:
     int front;
     int rear;
     int arr[MAX_SIZE];
 
 public:
-    Queue(){
+    Queue()
+    {
         front = -1;
         rear = -1;
     }
 
-    bool isFull(){
+    bool isFull()
+    {
         return (rear == MAX_SIZE - 1);
     }
-    bool isEmpty(){
+    bool isEmpty()
+    {
         return (front == -1 && rear == -1);
     }
 
-    void enqueue(int x){
-        if(isFull()){
+    void enqueue(int x)
+    {
+        if (isFull())
+        {
             cout << "Error: Queue is full" << endl;
             return;
         }
-        if(front == MAX_SIZE){
+        if (front == MAX_SIZE)
+        {
             front = 0;
         }
 
-        if(isEmpty()){
+        if (isEmpty())
+        {
             front = 0;
             rear = 0;
-        }else{
+        }
+        else
+        {
             rear++;
         }
         arr[rear] = x;
     }
-    void dequeue(){
-        if(isEmpty()){
+    void dequeue()
+    {
+        if (isEmpty())
+        {
             cout << "Error: Queue is empty" << endl;
             return;
         }
-        if(front == rear){
-            front  = -1;
+        if (front == rear)
+        {
+            front = -1;
             rear = -1;
-        }else{
+        }
+        else
+        {
             front++;
         }
     }
-    int peek(){
-        if(isEmpty()){
+    int peek()
+    {
+        if (isEmpty())
+        {
             cout << "Error: Queue is empty" << endl;
             return -1;
         }
         return arr[front];
     }
 
-    void display(){
-        if(isEmpty()){
+    void display()
+    {
+        if (isEmpty())
+        {
             cout << "Error: Queue is empty" << endl;
             return;
         }
 
-        cout<< "Queue elements are: ";
-        for(int i = front;i<=rear;i++){
+        cout << "Queue elements are: ";
+        for (int i = front; i <= rear; i++)
+        {
             cout << arr[i] << " ";
         }
         cout << endl;
-
     }
 };
 
-int main(){
+int main()
+{
     cout << "Initialize a Queue" << endl;
     Queue q;
     cout << "Is this Queue is empty? " << q.isEmpty() << endl;
@@ -86,9 +106,9 @@ int main(){
     q.enqueue(5);
     q.enqueue(6);
     cout << "Is the Queue full? " << q.isFull() << endl;
-    cout << "Is the Queue is empty? " << q.isEmpty()<<endl;
+    cout << "Is the Queue is empty? " << q.isEmpty() << endl;
     q.display();
-    cout << "Front element is "<< q.peek() << endl;
+    cout << "Front element is " << q.peek() << endl;
     cout << "Remove two elements from the Queue" << endl;
     q.dequeue();
     q.dequeue();
